6339082fc3ab8077f248c2484f8e427d94fbf81d
root <root@ANUBIS.anukis.local>
2019-04-22 11:50:04 +0200

    Change Name

---
+++ MAX-vendor-blobs.mk
@@ -0,0 +1,26 @@
+
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/etc/.tp,system/etc/.tp)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/etc/firmware,system/etc/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/etc/mddb,system/etc/mddb)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/vendor/lib/,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/vendor/lib/drm,system/vendor/lib/drm)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/vendor/lib/mediadrm,system/vendor/lib/mediadrm)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/vendor/lib64/mediadrm,system/vendor/lib64/mediadrm)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/MAX/xbin,system/xbin)
+
